11a7e843debee7c7c126f5fe104b16d93e2c3c2e
Thomas <84379061+KomaedaN@users.noreply.github.com>
2022-09-20 11:38:47 +0200

    Update get_csv_data() , create calcul_profit()

--- bruteforce.py
+++ bruteforce.py
@@ -2,15 +2,31 @@ import csv
 
 
 def get_csv_data(url):
+    """
+    get_csv_data read and get csv data from a file
+
+    :cost: cost data from csv file
+    :benefit: benefit data from csv file
+    :profit: calculate profit
+    :return: return all data inside a list
+    """
+
     with open(url) as file_csv:
         reader = csv.reader(file_csv, delimiter=",")
         data_list = []
         for row in reader:
             cost = float(row[1])
             benefit = float(row[2])
-            profit = (cost * benefit) / 100  # profit result
+            profit = calcul_profit(cost, benefit)
             data_list.append((row[0], cost, benefit, profit))
         return data_list
+
+
+def calcul_profit(cost, benefit):
+    """
+    :return: profit result
+    """
+    return (cost * benefit) / 100
 
 
 get_csv_data("data/dataset0.csv")
